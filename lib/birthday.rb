5fe81724fe7c73ec791e79de46921c9005c2173e
Dena Neuman <denaweiss5@gmail.com>
2020-10-15 17:26:29 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,5 +8,7 @@ def happy_birthday(birthday_kids)
   birthday_message=""
   birthday_kids.each do |kids_name, age|
     puts "Happy Birhtday #{kids_name}! You are now #{age} years old!"
-  end 
+  end
+end
+birthday_message
 end
